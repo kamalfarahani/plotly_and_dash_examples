db249c4009060a056a06368d6b06eb54ec45aeda
Kamal <kamal.farahani90@gmail.com>
2024-08-06 20:12:05 +0330

    add assigment 2

---
+++ 1-intro_plotly_dash/assignments/2.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import plotly.express as px
+
+from dash import Dash, html, dcc
+from dash.dependencies import Input, Output
+
+
+gdp_df = pd.read_csv("./data/gdp.csv")
+countries = gdp_df["Country Name"].unique()
+
+app = Dash(__name__)
+app.layout = html.Div(
+    [
+        html.P("Select Country", style={"font-weight": "bold"}),
+        dcc.Dropdown(
+            id="dropdown",
+            options=[{"label": country, "value": country} for country in countries],
+            value=countries[0],
+            style={"width": 500},
+        ),
+        dcc.Graph(id="graph"),
+    ]
+)
+
+
+@app.callback(
+    Output("graph", "figure"),
+    [Input("dropdown", "value")],
+)
+def update_graph(country):
+    years = gdp_df.columns[2:]
+    gdps = gdp_df[gdp_df["Country Name"] == country].iloc[0].values[2:]
+
+    df = pd.DataFrame(
+        {
+            "years": years,
+            "gdps": gdps,
+        }
+    )
+
+    fig = px.line(
+        df,
+        x="years",
+        y="gdps",
+    )
+    return fig
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
