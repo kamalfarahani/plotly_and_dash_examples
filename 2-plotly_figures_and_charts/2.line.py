aae4e7e8e83cb0f16d01b01ed421c84a108ce30d
Kamal <kamal.farahani90@gmail.com>
2024-08-07 20:51:04 +0330

    add line graph example

---
+++ 2-plotly_figures_and_charts/2.line.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+
+def load_data() -> pd.DataFrame:
+    stocks_df = px.data.stocks()
+
+    goog = stocks_df[["date", "GOOG"]]
+    aapl = stocks_df[["date", "AAPL"]]
+    amzn = stocks_df[["date", "FB"]]
+    nflx = stocks_df[["date", "NFLX"]]
+    msft = stocks_df[["date", "MSFT"]]
+
+    goog["type"] = "GOOG"
+    aapl["type"] = "AAPL"
+    amzn["type"] = "AMZN"
+    nflx["type"] = "NFLX"
+    msft["type"] = "MSFT"
+
+    all_data = np.vstack(
+        (
+            goog.values,
+            aapl.values,
+            amzn.values,
+            nflx.values,
+            msft.values,
+        )
+    )
+
+    data = pd.DataFrame(
+        all_data,
+        columns=["date", "value", "type"],
+    )
+    data["date"] = pd.to_datetime(data["date"])
+
+    return data
+
+
+def main() -> None:
+    data_df = load_data()
+    fig = px.line(
+        data_df,
+        x="date",
+        y="value",
+        color="type",
+    )
+
+    fig.update_layout(
+        title="Stocks",
+        xaxis_title="Date",
+        yaxis_title="Value",
+    )
+
+    fig.show()
+
+
+if __name__ == "__main__":
+    main()
